65d19e6cb4540762e1cf50be0622d0d460fbc201
lisiur <lisiurday@gmail.com>
2024-06-10 22:09:38 +0800

    init

---
+++ src/router/index.ts
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'index',
+      component: () => import('../views/IndexView'),
+      children: [
+        {
+          path: 'home',
+          name: 'home',
+          component: () => import('../views/HomeView')
+        },
+        {
+          path: 'friends',
+          name: 'friends',
+          component: () => import('../views/FriendsView')
+        },
+        {
+          path: 'airdrop',
+          name: 'airdrop',
+          component: () => import('../views/AirdropView')
+        }
+      ]
+    }
+  ]
+})
+
+export default router
---
+++ src/views/AirdropView.tsx
@@ -0,0 +1,8 @@
+import { defineComponent } from 'vue'
+import { Button } from 'vant'
+
+export default defineComponent({
+  setup() {
+    return () => <Button>TEST</Button>
+  }
+})
